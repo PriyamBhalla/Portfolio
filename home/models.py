3ac1138602204fff693b594e996d499d3b5d8645
Priyam <priyambhalla1998@gmail.com>
2022-02-01 14:48:26 +0530

    Updated Model and index.html

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
+from email import message
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    subject= models.CharField(max_length=30)
+    message= models.TextField()
+
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        subject=request.POST['subject']
+        message=request.POST['message']
+        Contact.save()
+        message.success("Your message has been sent.")
     return render(request,'index.html')
